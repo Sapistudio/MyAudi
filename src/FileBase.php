0bef03e636052d549c2935c85c0cba58291ab3ac
laur sandu <Sapistudio@users.noreply.github.com>
2018-09-13 01:03:35 +0300

    Update FileBase.php

--- src/FileBase.php
+++ src/FileBase.php
@@ -4,7 +4,7 @@ use \Lazer\Classes\Database;
 use \Lazer\Classes\Helpers\Validate;
 use \Lazer\Classes\LazerException as FileException;
 
-class FileBase
+class FileBase extends Database
 {
     /**
      * FileBase::loadDatabase()
@@ -17,15 +17,20 @@ class FileBase
             Validate::table($databaseName)->exists();
         }catch(FileException $e){
             try{
-                $databaseConfig = require __dir__.DIRECTORY_SEPARATOR.'configs'.DIRECTORY_SEPARATOR.$databaseName.'.config.php';
-                if(!$databaseConfig)
+                $configFile = __dir__.DIRECTORY_SEPARATOR.'configs'.DIRECTORY_SEPARATOR.$databaseName.'.config.php';
+                if(!file_exists($configFile))
                     return false;
-                Database::create($databaseName, $databaseConfig);
+                $databaseConfig = require __dir__.DIRECTORY_SEPARATOR.'configs'.DIRECTORY_SEPARATOR.$databaseName.'.config.php';  
+                self::create($databaseName, $databaseConfig);
             }catch(FileException $e){
                 return false;
             }
         }
-        return Database::table($databaseName);
+        $self       = new self();
+        $self->name = $databaseName;
+        $self->setFields();
+        $self->setPending();
+        return $self;
     }
     
     /**
@@ -34,8 +39,36 @@ class FileBase
      * @return void
      */
     public static function setDatabasePath(){
-        if (!defined('LAZER_DATA_PATH')) {
+        if (!defined('LAZER_DATA_PATH')){
             define('LAZER_DATA_PATH', realpath(__DIR__).DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR);//file system database path
         }
+    }
+    
+    /**
+     * FileBase::addEntry()
+     * 
+     * @param mixed $data
+     * @return void
+     */
+    public function addEntry($data)
+    {
+        foreach ($data as $name => $value){
+            if (Validate::table($this->name)->field($name) && Validate::table($this->name)->type($name,$value))
+                $this->set->{$name} = utf8_encode($value);
+        }
+        $this->save();
+    }
+    
+    /**
+     * FileBase::addEntry()
+     * 
+     * @param mixed $data
+     * @return void
+     */
+    public function getEntry($entryId = null)
+    {
+        $data = (is_int($entryId)) ? (array)$this->find($entryId)->set : $this->asArray();
+        $this->clearQuery();
+        return json_decode(json_encode($data));
     }
 }
