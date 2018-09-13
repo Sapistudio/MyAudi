8698c2f9ae45fe4bb9e9687d5b06c0cc9bdaa14f
laur sandu <Sapistudio@users.noreply.github.com>
2018-09-13 04:36:23 +0300

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -61,20 +61,23 @@ class Handler extends AbstractHttpClient
         $database       = FileBase::loadDatabase(DatabaseConfig::LOCATION_DATABASE);
         $lastEntry      = $database->getEntry($database->lastId());
         $parkingTime    = date("Y-m-d H:i", strtotime($carResponse->parkingTimeUTC));
-        $fields = ['0x030103000A','0x0301030005',''];
-        $locationEntry = [
-            'dateupdated'   => date("Y-m-d H:i"), 
-            'startinglat'   => self::coordinateConverter($lastEntry->endinglat),
-            'startinglon'   => self::coordinateConverter($lastEntry->endinglon),
-            'endinglat'     => self::coordinateConverter($carResponse->Position->carCoordinate->latitude),
-            'endinglon'     => self::coordinateConverter($carResponse->Position->carCoordinate->longitude),
-            'parkingtime'   => date("Y-m-d H:i", strtotime($carResponse->parkingTimeUTC)),
-        ];
+        $locationEntry  = ['dateupdated'   => date("Y-m-d H:i")];
+        $status         = VehicleStatus::initStatus($this->status());
         if(isset($lastEntry->parkingtime) && $lastEntry->parkingtime == $parkingTime){
             $locationEntry[DatabaseConfig::UNIQUE_IDENTIFIER] = $lastEntry->{DatabaseConfig::UNIQUE_IDENTIFIER};
+        }else{
+            if(isset($lastEntry->endinglat)){
+                $locationEntry['startinglat'] = $lastEntry->endinglat;
+                $locationEntry['startinglon'] = $lastEntry->endinglon;
+            }
+            $locationEntry['endinglat'] = self::coordinateConverter($carResponse->Position->carCoordinate->latitude);
+            $locationEntry['endinglon'] = self::coordinateConverter($carResponse->Position->carCoordinate->longitude);
+            $locationEntry['parkingtime'] = $parkingTime;
+            $locationEntry['fuelprocent'] = $status->getFieldData('0x030103000A');
+            $locationEntry['currentmilleage'] = $status->getFieldData('0x0101010002');
+            $locationEntry['remainingmilleage'] = $status->getFieldData('0x0301030005');
         }
-        print_R($locationEntry);die();
-        $database->addEntry();
+        $database->addEntry($locationEntry);
         return $carResponse;
     }
     
@@ -145,8 +148,6 @@ class Handler extends AbstractHttpClient
      * @return
      */
     public static function coordinateConverter($coordinateNumber = null){
-        if(!$coordinateNumber)
-            return null;
         $dot = strlen($coordinateNumber)-6;
         return substr($coordinateNumber,0,$dot).'.'.substr($coordinateNumber, $dot);
     }
