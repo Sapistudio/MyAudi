90ea0a8535a04ac8bdca6d0a36fe30ccc4949d0c
laur sandu <Sapistudio@users.noreply.github.com>
2020-06-01 22:50:20 +0300

    Delete DatabaseConfig.php

--- src/DatabaseConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace SapiStudio\AudiMMI;
-use \SapiStudio\RestApi\AbstractHttpClient;
-
-class DatabaseConfig
-{
-    const HISTORY_DATABASE  = 'vehicleHistory'; 
-    const UNIQUE_IDENTIFIER = 'id'; 
-}
