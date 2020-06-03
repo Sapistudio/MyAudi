d70551273962702beb8548620935ab88a95c263a
laur sandu <Sapistudio@users.noreply.github.com>
2020-06-04 02:00:05 +0300

    Create apiservice.todo.php

---
+++ src/configs/apiservice.todo.php
@@ -0,0 +1,22 @@
+<?php 
+return [
+        
+        /** TO DO
+        "rolesrights"   => [
+            //"url"           => "fs-car/rolesrights/operationlist/v2/{brandType}/{country}/vehicles/{vin}/operations",
+            "url"             => "myaudi/rolesrights/v1/vehicles/{vin}"
+        ],
+        "newsFeeds"   => [
+            "url"           => "fs-car/news/myfeeds/v1{brandType}/{country}/newsFeeds",
+        ],
+        "requests" => [
+            "url"       => "fs-car/bs/vsr/v1/{brandType}/{country}/vehicles/{vin}/requests",
+            "element"   => ""
+        ],
+        "socialTwitter"   => [
+            "url"           => "fs-car/social/twitter/v1/{brandType}/{country}/vehicles/{vin}/credentials",
+        ],
+        */
+        //DELETE https://msg.audi.de/myaudi/profileservice/v1/entries/01186e1f-7fe7-48b9-a397-57f3e7a95376?type=CostItem
+    ];
+;
